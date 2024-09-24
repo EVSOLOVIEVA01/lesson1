353f473a6828da6cdbcaba4679abbe579e1e6bed
EVSOLOVIEVA01 <hottabisch1@gmail.com>
2024-09-24 22:06:38 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,74 @@
+"1st program"
+print(9 ** 0.5 * 5)
+"2nd program"
+print(9.99 > 9.98) and (1000 != 1000.1)
+"3rd program"
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+var1 = (2 * 2 + 2)
+var2 = (2 * (2 + 2))
+print(var1 == var2)
+"4th program"
+var3 = int((float('123.456') * 10 % 10))
+print(var3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
